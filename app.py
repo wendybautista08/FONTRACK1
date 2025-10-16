228f1d9b551ae32139b132f775412974b8e39587
Dominnyck <dominnyck35@gmail.com>
2025-10-16 17:45:18 -0500

    actualización

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def home():
-    return render_template("home.html")
+    return render_template('home.html')
 
-if __name__ == "__main__":
+@app.route('/pago')
+def pago():
+    return render_template('pago.html')
+
+@app.route('/procesar_pago', methods=['POST'])
+def procesar_pago():
+    nombre = request.form['nombre']
+    email = request.form['email']
+    metodo = request.form['metodo']
+    print(f"Pago recibido de {nombre}, Email: {email}, Método: {metodo}")
+    return redirect(url_for('home'))
+
+if __name__ == '__main__':
     app.run(debug=True)
